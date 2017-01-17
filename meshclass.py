cb5d4f78d0f5247d3d7be27a0f1b53adcdf7bf17
jswien <jswien@gmail.com>
2017-01-17 12:12:01 -0800

    Added Npsi metric and cleaned up code

--- meshclass.py
+++ meshclass.py
@@ -22,12 +22,14 @@ class Mesh(object):
         
         self.coors = self._build_coors(makemesh_output)
         self.mesh_length = self.coors.size/2
+        
         self.elements_to_nodes = self._build_elements_to_nodes(makemesh_output)
         self.nodes_to_elements = self._build_nodes_to_elements(makemesh_output)
         self.bndy_elements = self._build_bndy_elements(makemesh_output)
         self.neighborhoods = self._build_neighborhoods()
 
-        self.psi_metric = self._build_psi_metric()
+        self.psi_metric=self._build_global_metric(local_psi_metric)
+        self.Npsi_metric=self._build_global_metric(local_Npsi_metric)
 
     def _call_makemesh(self, *arguments):
         """Run the makemesh script"""
@@ -73,10 +75,11 @@ class Mesh(object):
                                       block in neighborhoods[node]for
                                       elem in block])
         return neighborhoods
-            
-    def _build_psi_metric(self):
-        """Build matrix of integrals psi_i psi_j"""
-        psi_metric = np.zeros((self.mesh_length,self.mesh_length))
+    
+    
+    def _build_global_metric(self, metric_function):
+        """Build global metric from local function"""
+        metric = np.zeros((self.mesh_length,self.mesh_length))
         for n_i in xrange(self.mesh_length):
             for n_j in self.neighborhoods[n_i]:
                 for elem in list(set(self.nodes_to_elements[n_i])&
@@ -84,14 +87,13 @@ class Mesh(object):
                                  ):
                     n_i_pos = self.elements_to_nodes[elem].index(n_i)
                     n_j_pos = self.elements_to_nodes[elem].index(n_j)
-                    psi_metric[n_i,n_j]+=local_psi_metric(
+                    metric[n_i,n_j]+=metric_function(
                         np.array([self.coors[node] for node 
                                   in self.elements_to_nodes[elem][:3]
                                  ])
                         )[n_i_pos,n_j_pos]
-        return psi_metric
-
-
+        return metric
+                               
 def local_psi_metric(vertices):
     jac = np.absolute(vertices[0,0]*(vertices[1,1]-vertices[2,1])
                       +vertices[1,0]*(vertices[2,1]-vertices[0,1])
@@ -110,6 +112,47 @@ def local_psi_metric(vertices):
                      [0,vn,0,nnp,nnp,nn],
                      ])
 
-mesh = Mesh(np.array([[1,0,0],[0,1,0],[0,0,-1]]), np.array([0.005,6]))
+def local_Npsi_metric(vertices):
+    jac = np.absolute(vertices[0,0]*(vertices[1,1]-vertices[2,1])
+                      +vertices[1,0]*(vertices[2,1]-vertices[0,1])
+                      +vertices[2,0]*(vertices[0,1]-vertices[1,1])
+                      )
+    area = 4*((vertices[0,0]-vertices[1,0])**2
+              +(vertices[0,0]-vertices[2,0])**2
+              +(vertices[2,0]-vertices[1,0])**2
+              +(vertices[0,1]-vertices[1,1])**2
+              +(vertices[0,1]-vertices[2,1])**2
+              +(vertices[2,1]-vertices[1,1])**2
+             )
+    c00 = ((vertices[1,0]-vertices[2,0])**2
+           +(vertices[1,1]-vertices[2,1])**2)
+    c11 = ((vertices[0,0]-vertices[2,0])**2
+           +(vertices[0,1]-vertices[2,1])**2)
+    c22 = ((vertices[0,0]-vertices[1,0])**2
+           +(vertices[0,1]-vertices[1,1])**2)
+    c01 = ((vertices[0,0]-vertices[2,0])
+           *(vertices[1,0]-vertices[2,0])
+           +(vertices[0,1]-vertices[2,1])
+           *(vertices[1,1]-vertices[2,1]))
+    c02 = ((vertices[0,0]-vertices[1,0])
+           *(vertices[2,0]-vertices[1,0])
+           +(vertices[0,1]-vertices[1,1])
+           *(vertices[2,1]-vertices[1,1]))
+    c12 = ((vertices[1,0]-vertices[0,0])
+           *(vertices[2,0]-vertices[0,0])
+           +(vertices[1,1]-vertices[0,1])
+           *(vertices[2,1]-vertices[0,1]))
+    
+    return 1/jac*1/6*np.array([[3*c00,c01,c02,-4*c01,0,-4*c02],
+                               [c01,3*c11,c12,-4*c01,-4*c12,0],
+                               [c02,c12,3*c22,0,-4*c12,-4*c02],
+                               [-4*c01,-4*c01,0,area,-8*c02,-8*c12],
+                               [0,-4*c12,-4*c12,-8*c02,area,-8*c01],
+                               [-4*c02,0,-4*c02,-8*c12,-8*c01,area],
+                               ])
+
+mesh = Mesh(np.array([[1,0,0],[0,1,0],[0,0,-1]]), np.array([0.005,4]))
 
 print np.ones(mesh.mesh_length).dot(mesh.psi_metric.dot(np.ones(mesh.mesh_length)))-np.pi/4
+
+print np.ones(mesh.mesh_length).dot(mesh.Npsi_metric.dot(np.ones(mesh.mesh_length)))
